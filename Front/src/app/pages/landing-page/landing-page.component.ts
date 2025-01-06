7b58da782bad4fe71311f68377c2290c4c1c0da5
sebastienDOC <s.dechand21@gmail.com>
2025-01-06 18:32:13 +0100

    feat(landing-page): add footer

--- Front/src/app/pages/landing-page/landing-page.component.ts
+++ Front/src/app/pages/landing-page/landing-page.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 import { AboutComponent } from "../../components/about/about.component";
-import { HeaderComponent } from "../../components/header/header.component";
-import { ServicesComponent } from "../../components/services/services.component";
-import { PhotoGalleryComponent } from "../../components/photo-gallery/photo-gallery.component";
 import { CreatorsGalleryComponent } from "../../components/creators-gallery/creators-gallery.component";
 import { CustomShoppingComponent } from "../../components/custom-shopping/custom-shopping.component";
+import { PhotoGalleryComponent } from "../../components/photo-gallery/photo-gallery.component";
 import { ReviewsComponent } from "../../components/reviews/reviews.component";
+import { ServicesComponent } from "../../components/services/services.component";
 
 @Component({
   selector: 'app-landing-page',
-  imports: [HeaderComponent, ServicesComponent, AboutComponent, PhotoGalleryComponent, CreatorsGalleryComponent, CustomShoppingComponent, ReviewsComponent],
+  imports: [ServicesComponent, AboutComponent, PhotoGalleryComponent, CreatorsGalleryComponent, CustomShoppingComponent, ReviewsComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
