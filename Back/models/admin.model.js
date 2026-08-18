c4598995a7952f42af25f43c6517b9460ce46275
sebastienDOC <s.dechand21@gmail.com>
2026-08-18 17:51:17 +0200

    feat: admin changes

--- Back/models/admin.model.js
+++ Back/models/admin.model.js
@@ -32,16 +32,11 @@ const AdminSchema = new mongoose.Schema({
   timestamps: true
 });
 
-AdminSchema.pre('save', async function(next) {
-  if (!this.isModified('password')) return next();
-  
-  try {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-  } catch (error) {
-    next(error);
-  }
+AdminSchema.pre('save', async function() {
+  if (!this.isModified('password')) return;
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 AdminSchema.methods.comparePassword = async function(candidatePassword) {
